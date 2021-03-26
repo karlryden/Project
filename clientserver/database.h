a54c3c831e75219131eeab85b798252c528b1062
karlryden <karlrydenn@gmail.com>
2021-03-26 16:12:55 +0100

    :)

--- clientserver/database.h
+++ clientserver/database.h
@@ -1,7 +1,12 @@
+#include "newsgroup.h"
 #include <string>
 
 class Database {
     public:
-        virtual Article get_article(unsigned int, ?);
-        virtual NewsGroup get_newsgroup(?);
+        virtual Article get_article(unsigned int, unsigned int) = 0;
+        virtual NewsGroup get_newsgroup(unsigned int) = 0;
+        virtual bool set_article(?, ?) = 0;
+        virtual bool set_newsgroup(?, ?) = 0;
+        virtual bool remove_article(unsigned int, unsigned int) = 0;
+        virtual bool remove_newsgroup(unsigned int, unsigned int) = 0;
 };
--- clientserver/newsserver.h
+++ clientserver/newsserver.h
@@ -4,7 +4,9 @@
 class NewsServer : public Server {
     public:
         NewsServer();
-        NewsServer(Database);
+        NewsServer(Database&);
+        std::string get_newsgroup(unsigned int);
+        std::string get_article(unsigned int, unsigned int);
         bool create_newsgroup(std::string);
         bool create_article(std::string, std::string, std::string, unsigned int);
         bool remove_newsgroup(unsigned int);
@@ -12,5 +14,5 @@ class NewsServer : public Server {
         std::string list_newsgroups();
         std::string list_articles(unsigned int);
     private:
-        Database db;
+        Database& db;
 };
