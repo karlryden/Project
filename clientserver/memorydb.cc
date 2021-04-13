b01932a15ade310d316385f1cee578f667e48390
karlryden <karlrydenn@gmail.com>
2021-04-13 16:56:04 +0200

    memorydb klar?

--- clientserver/memorydb.cc
+++ clientserver/memorydb.cc
@@ -2,25 +2,25 @@
 #include <algorithm>
 
 using std::string;
+using std::vector;
 using std::find;
 using std::find_if;
 
-string MemoryDatabase::get_newsgroup(string name) const {
-    NewsGroup ng{*find_if(table.begin(), table.end(), [name](const NewsGroup& n) {return n.name == name;})};
+string MemoryDatabase::get_newsgroup(unsigned int ng_id) const {
+    NewsGroup ng{*find_if(table.begin(), table.end(), [ng_id](const NewsGroup& n) {return n.id == ng_id;})};
     
     return ng.to_string();
 }
 
-string MemoryDatabase::get_article(string name, string title, string author) const {
-    NewsGroup ng{*find_if(table.begin(), table.end(), [name](const NewsGroup& n) {return n.name == name;})};
-    Article a{ng.get_article(title, author)};
+string MemoryDatabase::get_article(unsigned int ng_id, unsigned int a_id) const {
+    NewsGroup ng{*find_if(table.begin(), table.end(), [ng_id](const NewsGroup& n) {return n.id == ng_id;})};
+    Article a{ng.get_article(a_id)};
 
     return a.to_string();
 }
 
 bool MemoryDatabase::set_newsgroup(string name) {
     NewsGroup ng{name};
-    // return (table.insert(ng)).second;
     table.push_back(ng);    
     
     return true;
@@ -28,15 +28,15 @@ bool MemoryDatabase::set_newsgroup(string name) {
 
 bool MemoryDatabase::set_article(string name, string tit, string aut, string tex) {
     // NewsGroup ng{*(table.find(ng_id))};
-    NewsGroup ng{*find_if(table.begin(), table.end(), [name](const NewsGroup& n) {return n.name == name;})};
+    NewsGroup ng{*find_if(table.begin(), table.end(), [name](const NewsGroup& n) {return n.get_name() == name;})};
     Article a(tit, aut, tex);
 
     return ng.set_article(a);
 }
 
-bool MemoryDatabase::remove_newsgroup(string name) {
+bool MemoryDatabase::remove_newsgroup(unsigned int ng_id) {
     // return table.erase(ng_id);
-    auto pos{find_if(table.begin(), table.end(), [name](const NewsGroup& n) {return n.name == name;})};
+    auto pos{find_if(table.begin(), table.end(), [ng_id](const NewsGroup& n) {return n.id == ng_id;})};
     if (pos != table.end()) {
         table.erase(pos);
         return true;
@@ -45,18 +45,20 @@ bool MemoryDatabase::remove_newsgroup(string name) {
     return false;
 }
 
-bool MemoryDatabase::remove_article(string name, string tit, string aut) {
-    NewsGroup ng{*find_if(table.begin(), table.end(), [name](const NewsGroup& n) {return n.name == name;})};
+bool MemoryDatabase::remove_article(unsigned int ng_id, unsigned int a_id) {
+    NewsGroup ng{*find_if(table.begin(), table.end(), [ng_id](const NewsGroup& n) {return n.id == ng_id;})};
     
-    return ng.remove_article(tit, aut);
+    return ng.remove_article(a_id);
 }
-vector::iterator MemoryDatabase::begin(){
+vector<NewsGroup>::iterator MemoryDatabase::begin(){
     return table.begin();
 }
-vector::iterator MemoryDatabase::end(){
+
+vector<NewsGroup>::iterator MemoryDatabase::end(){
     return table.end();
 }
-int MemoryDatabase::size(){
-    return table.size()
+
+int MemoryDatabase::size() {
+    return static_cast<int>(table.size());
 }
 
--- clientserver/memorydb.h
+++ clientserver/memorydb.h
@@ -6,16 +6,16 @@
 
 class MemoryDatabase : public Database {
 public:
-    std::string get_newsgroup(std::string) const;
-    std::string get_article(std::string, std::string, std::string) const;
+    std::string get_newsgroup(unsigned int) const;
+    std::string get_article(unsigned int, unsigned int) const;
     bool set_newsgroup(std::string);
     bool set_article(std::string, std::string, std::string, std::string);
-    bool remove_newsgroup(std::string);
-    bool remove_article(std::string, std::string, std::string);
+    bool remove_newsgroup(unsigned int);
+    bool remove_article(unsigned int, unsigned int);
     //
     std::vector<NewsGroup>::iterator begin();
     std::vector<NewsGroup>::iterator end();
-    std::vector<NewsGroup>::size_type size();
+    int size();
 
 private:
         std::vector<NewsGroup> table;
