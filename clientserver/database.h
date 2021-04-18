c29022d2dc782f8c7d28ca24d0202eabbe5de157
snelhest1337 <em2803ma-s@student.lu.se>
2021-04-18 14:37:57 +0200

    lite dokumentation

--- clientserver/database.h
+++ clientserver/database.h
@@ -6,12 +6,18 @@
 
 class Database {
     public:
+        // returnar tom sträng om article/newsgroup not found
+        // newsgroup: id namn
         virtual std::string get_newsgroup(unsigned int) const = 0;
+
+        // article: title|author|text
         virtual std::string get_article(unsigned int, unsigned int) const = 0;
         virtual bool set_newsgroup(std::string) = 0;
         virtual bool set_article(unsigned int, std::string, std::string, std::string) = 0;
         virtual bool remove_newsgroup(unsigned int) = 0;
         virtual bool remove_article(unsigned int, unsigned int) = 0;
+
+        //lista:  antal id längd titel id längd titel
         virtual std::string list_articles(unsigned int) = 0;
         virtual std::string list_newsgroups() = 0;
 };
